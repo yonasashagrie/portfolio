e33df5f1ed85049ff397a2b9ba4394f6945b9b72
yonasashagrie <149861216+yonasashagrie@users.noreply.github.com>
2024-05-12 06:27:53 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -171,6 +171,18 @@ VanillaTilt.init(document.querySelectorAll(".tilt"), {
 // window.onload = fadeOut;
 // pre loader end
 
+// Start of Tawk.to Live Chat
+var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+(function () {
+    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+    s1.async = true;
+    s1.src = 'https://embed.tawk.to/6640bce79a809f19fb300894/1htme3qvp';
+    s1.charset = 'UTF-8';
+    s1.setAttribute('crossorigin', '*');
+    s0.parentNode.insertBefore(s1, s0);
+})();
+// End of Tawk.to Live Chat
+
 // disable developer mode
 document.onkeydown = function (e) {
     if (e.keyCode == 123) {
