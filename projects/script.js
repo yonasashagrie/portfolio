b29cf417c71a2f1ec58c5d576862a2c58e4dd9ad
yonasashagrie <149861216+yonasashagrie@users.noreply.github.com>
2024-05-12 06:15:14 -0700

    update

--- projects/script.js
+++ projects/script.js
@@ -105,18 +105,6 @@ getProjects().then(data => {
 })
 // fetch projects end
 
-//<!--Start of Tawk.to Script-->
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/6640bce79a809f19fb300894/1htme3qvp';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
-
-//<!--End of Tawk.to Script-->
 
 // disable developer mode
 document.onkeydown = function (e) {
